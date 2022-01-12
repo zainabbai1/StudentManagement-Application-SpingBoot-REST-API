c9cc5931b8d1ffb8bc9444fac671098659b421f2
Mustaq Sait <zainabbai1997@gmail.com>
2022-01-12 20:06:22 +0530

    first commit

---
+++ student management app.sql
@@ -0,0 +1,6 @@
+create database sms;
+
+use sms;
+
+create table students(id bigint primary key auto_increment,first_name varchar(255),last_name varchar(255),email varchar(255));
+select*from students;
